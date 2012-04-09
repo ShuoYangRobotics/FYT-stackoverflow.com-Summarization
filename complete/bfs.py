210f599430725d359f3d64838a5fe434a6145790
Paul Yang <paulyang1990@gmail.com>
2012-04-09 14:07:00 +0800

    The bfs.py contains a BFS function that can search graph

---
+++ complete/bfs.py
@@ -0,0 +1,45 @@
+from collections import deque
+
+def bfs(g, start):
+    queue, enqueued = deque([(None, start)]), set([start])
+    while queue:
+        parent, n = queue.popleft()
+        yield parent, n
+        new = set(g[n]) - enqueued
+        enqueued |= new
+        queue.extend([(n, child) for child in new])
+
+def dfs(g, start):
+    stack, enqueued = [(None, start)], set([start])
+    while stack:
+        parent, n = stack.pop()
+        yield parent, n
+        new = set(g[n]) - enqueued
+        enqueued |= new
+        stack.extend([(n, child) for child in new])
+
+def shortestPath(g, start, end):
+    parents = {}
+    for parent, child in bfs(g, start):
+        parents[child] = parent
+        if child == end:
+            revpath = [end]
+            while True:
+                parent = parents[child]
+                revpath.append(parent)
+                if parent == start:
+                    break
+                child = parent
+            return list(reversed(revpath))
+    return None # or raise appropriate exception
+
+if __name__ == '__main__':
+    # a sample graph
+    graph = {'A': ['B', 'C','E'],
+             'B': ['A','C', 'D'],
+             'C': ['D'],
+             'D': ['C'],
+             'E': ['F', 'D'],
+             'F': ['C']}
+
+    print(shortestPath(graph, 'A', 'D'))
